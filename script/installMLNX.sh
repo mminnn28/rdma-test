c302191231e5c377e9c201a1ec807b10330bd224
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-03 21:57:32 +0900

    Update installMLNX.sh

--- script/installMLNX.sh
+++ script/installMLNX.sh
@@ -5,10 +5,10 @@ if [ ! -d "tmp" ]; then
 fi
 
 cd tmp
-wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.8-5.1.1.2/MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
-tar -xvf MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
+wget https://content.mellanox.com/ofed/MLNX_OFED-5.8-5.1.1.2/MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
+tar -xzvf MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
 cd MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64
 
-sudo ./mlnxofedinstall  --force
+sudo ./mlnxofedinstall --force
 sudo /etc/init.d/openibd restart
 sudo /etc/init.d/opensmd restart
