516b4ec5114ec3e4a0db7b04cabff1f4219219ad
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-03 21:36:49 +0900

    Update installMLNX.sh

--- script/installMLNX.sh
+++ script/installMLNX.sh
@@ -5,7 +5,7 @@ if [ ! -d "tmp" ]; then
 fi
 
 cd tmp
-wget https://content.mellanox.com/ofed/MLNX_OFED-5.8-5.1.1.2/MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
+wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.8-5.1.1.2/MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
 tar -xvf MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
 cd MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64
 
