bc0575c67620f130e8b67cda7c8197ec1ea4163c
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-04 15:05:48 +0900

    Update installMLNX2.sh

--- script/installMLNX2.sh
+++ script/installMLNX2.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+if [ ! -d "tmp" ]; then
+	mkdir tmp
+fi
+
+cd tmp
+wget https://content.mellanox.com/ofed/MLNX_OFED-4.9-7.1.0.0/MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu18.04-x86_64.tgz
+tar -xzvf MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu18.04-x86_64.tgz
+cd MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu18.04-x86_64
+
+sudo ./mlnxofedinstall --force
+sudo /etc/init.d/openibd restart
+sudo /etc/init.d/opensmd restart
