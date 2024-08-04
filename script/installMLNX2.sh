58d416d90746fff6eaad11f7b00ad60b78728a04
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-04 15:07:31 +0900

    Update installMLNX2.sh

--- script/installMLNX2.sh
+++ script/installMLNX2.sh
@@ -4,11 +4,6 @@ if [ ! -d "tmp" ]; then
 	mkdir tmp
 fi
 
-sudo su
-chsh -s /bin/bash
-apt-get update
-apt-get install rdma-core ibverbs-utils perftest
-
 cd tmp
 wget https://content.mellanox.com/ofed/MLNX_OFED-4.9-7.1.0.0/MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu18.04-x86_64.tgz
 tar -xzvf MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu18.04-x86_64.tgz
