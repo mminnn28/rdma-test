62e932a9759c9b7343e779fcd27847c899d2de3f
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-06 13:19:25 +0900

    Update installMLNX.sh

--- script/installMLNX.sh
+++ script/installMLNX.sh
@@ -4,21 +4,25 @@
 mode="$1"
 ubuntu_version=$(lsb_release -r -s)
 
+# Update and install necessary packages
 apt-get update -y
 apt-get install rdma-core ibverbs-utils perftest -y
 
-chsh -s /bin/bash
+# Change the default shell to /bin/bash for the current user
+chsh -s /bin/bash $USER
 
 if [ ! -d "tmp" ]; then
 	mkdir tmp
 fi
 
 cd tmp
-if [ $ubuntu_version == "18.04" ]; then
+
+# Download and extract the appropriate Mellanox OFED package
+if [ "$ubuntu_version" == "18.04" ]; then
   wget https://content.mellanox.com/ofed/MLNX_OFED-5.8-5.1.1.2/MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
   tar -xzvf MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64.tgz
   cd MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu18.04-x86_64
-elif [ $ubuntu_version == "22.04" ]; then
+elif [ "$ubuntu_version" == "22.04" ]; then
   wget https://content.mellanox.com/ofed/MLNX_OFED-5.8-5.1.1.2/MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu22.04-x86_64.tgz
   tar -xzvf MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu22.04-x86_64.tgz
   cd MLNX_OFED_LINUX-5.8-5.1.1.2-ubuntu22.04-x86_64
