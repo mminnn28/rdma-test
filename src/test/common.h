26f6a355a0b556bee1fdf5e1827389643343ae48
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-15 19:53:58 +0900

    Update common.h

--- src/test/common.h
+++ src/test/common.h
@@ -14,12 +14,13 @@
 #define BUFFER_SIZE (KEY_VALUE_SIZE * 2)
 #define SERVER_PORT 20079
 
+
 /* Capacity of the completion queue (CQ) */
 #define CQ_CAPACITY (16)
 /* MAX SGE capacity */
 #define MAX_SGE (1)
 /* MAX work requests */
-#define MAX_WR (10)
+#define MAX_WR (16)
 
 // Define message types
 enum msg_type {
@@ -27,14 +28,15 @@ enum msg_type {
     MSG_GET
 };
 
-// Define message structure
+struct kv_pair {
+    char key[KEY_VALUE_SIZE];
+    char value[KEY_VALUE_SIZE];
+};
+
 struct message {
     enum msg_type type;
-    struct {
-        char key[KEY_VALUE_SIZE];
-        char value[KEY_VALUE_SIZE];
-    } kv;
-    uintptr_t addr;
+    struct kv_pair kv;
+    uint64_t addr;  // 서버 메모리 주소
 };
 
 // RDMA context structure
@@ -45,19 +47,24 @@ struct rdma_context {
     struct ibv_comp_channel *comp_channel;
     struct ibv_cq *cq;
     struct ibv_qp *qp;
-    struct ibv_mr *mr;
-    void *buffer;
+    
+    struct ibv_mr * send_mr;
+	struct ibv_mr * recv_mr;
+
+    char *send_buffer;
+	char *recv_buffer;
+
+    pthread_t poll_send_thread;
+    pthread_t poll_recv_thread;
 };
 
 // Function prototypes
 void build_context(struct rdma_context *ctx);
 void build_qp_attr(struct ibv_qp_init_attr *attr, struct rdma_context *ctx);
-struct ibv_mr* rdma_buffer(struct rdma_context *ctx);
-void show_rdma_buffer_attr(struct rdma_buffer_attr *attr);
-int process_rdma_cm_event(struct rdma_event_channel *echannel, 
-		enum rdma_cm_event_type expected_event,
-		struct rdma_cm_event **cm_event);
-
-// void on_completion(struct ibv_wc *wc);
+struct void rdma_buffer(struct rdma_context *ctx);
+void recv_msg(struct rdma_context *ctx);
+void * poll_send_cq(void * context);
+void * poll_recv_cq(void * context);
+int on_completion(struct ibv_wc *wc);
 
 #endif // COMMON_H
