0afead3b52577f4e63129134697e47aa51e08acd
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-18 22:51:02 +0900

    Update common.h

--- src/test/common.h
+++ src/test/common.h
@@ -19,6 +19,17 @@
 // #define ntohll(x) __builtin_bswap64(x)
 // #endif //endian.h
 
+static inline uint64_t bswap_64(uint64_t x) {
+    return ((x & 0x00000000000000FFULL) << 56) |
+           ((x & 0x000000000000FF00ULL) << 40) |
+           ((x & 0x0000000000FF0000ULL) << 24) |
+           ((x & 0x00000000FF000000ULL) << 8)  |
+           ((x & 0x000000FF00000000ULL) >> 8)  |
+           ((x & 0x0000FF0000000000ULL) >> 24) |
+           ((x & 0x00FF000000000000ULL) >> 40) |
+           ((x & 0xFF00000000000000ULL) >> 56);
+}
+
 /* Error Macro*/
 #define rdma_error(msg, args...) do {\
 	fprintf(stderr, "%s : %d : ERROR : "msg, __FILE__, __LINE__, ## args);\
@@ -33,6 +44,11 @@
 #define MAX_SGE 1
 #define MAX_WR 16
 #define MAX_KEYS 256
+
+struct pdata { 
+    uint64_t buf_va; 
+    uint32_t buf_rkey; 
+};
 
 // Define message types
 enum msg_type {
@@ -71,6 +87,7 @@ struct rdma_context {
     struct ibv_pd *pd;
     struct ibv_comp_channel *comp_channel;
     struct ibv_cq *cq;
+    struct ibv_cq *evt_cq;
     struct ibv_qp *qp;
     struct ibv_mr *send_mr, *recv_mr;
 };
@@ -78,6 +95,6 @@ struct rdma_context {
 // Function prototypes
 void build_context(struct rdma_context *ctx, struct rdma_cm_id *id);
 void build_qp_attr(struct ibv_qp_init_attr *attr, struct rdma_context *ctx);
-void cleanup(struct rdma_cm_id *id);
 
 #endif // COMMON_H
+
