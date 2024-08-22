1c07fb29425fd669a72ea1e32ce051573ecc1c74
jeongeun Kim <rlawjd10eun@gmail.com>
2024-08-22 15:05:58 +0900

    Update client.c

--- src/test/client.c
+++ src/test/client.c
@@ -1,13 +1,14 @@
 #include "common.h"
 
-/* RDMA 관련 리소스 */
+/* RDMA resource */
 static struct rdma_context ctx;
 static struct rdma_cm_id *id = NULL;
 static struct rdma_event_channel *ec = NULL;
 static struct rdma_cm_event *event = NULL;
 static struct ibv_qp_init_attr qp_attr;
 
-static struct pdata client_pdata, server_pdata;
+void *cq_context;
+static struct pdata rep_pdata;
 
 struct ibv_recv_wr recv_wr, *bad_recv_wr = NULL;
 struct ibv_send_wr send_wr, *bad_send_wr = NULL;
@@ -18,11 +19,15 @@ static char *send_buffer = NULL, *recv_buffer = NULL;
 static void setup_connection(const char *server_ip);
 static void pre_post_recv_buffer();
 static void connect_server();
+
 int on_connect();
 void post_send_message();
-void receive_response();
+int receive_response();
+int wait_for_completion();
+int post_and_wait(struct ibv_send_wr *wr, const char *operation_name);
+
 void cleanup(struct rdma_cm_id *id);
-int prepare_send_notify_after_rdma_write();
+
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -41,7 +46,6 @@ int main(int argc, char **argv) {
 static void setup_connection(const char *server_ip) {
     int ret;
     struct sockaddr_in addr;
-
 
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
@@ -108,22 +112,22 @@ static void setup_connection(const char *server_ip) {
         exit(EXIT_FAILURE);
     }
 }
+
 static void pre_post_recv_buffer() {
-    int ret = -1;
-    recv_buffer = (char *)malloc(BUFFER_SIZE);
+    recv_buffer = calloc(2, sizeof(uint32_t));
     if (!recv_buffer) {
         perror("Failed to allocate memory for receive buffer");
         exit(EXIT_FAILURE);
     }
 
-    ctx.recv_mr = ibv_reg_mr(ctx.pd, recv_buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE);
+    ctx.recv_mr = ibv_reg_mr(ctx.pd, recv_buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE);
     if (!ctx.recv_mr) {
         perror("Failed to register receive memory region");
         exit(EXIT_FAILURE);
     }
 
-    recv_sge.addr = (uint64_t)recv_buffer;
-    recv_sge.length = BUFFER_SIZE;
+    recv_sge.addr =  (uintptr_t)recv_buffer;
+    recv_sge.length = sizeof(uint32_t);
     recv_sge.lkey = ctx.recv_mr->lkey;
 
     memset(&recv_wr, 0, sizeof(recv_wr));
@@ -131,8 +135,7 @@ static void pre_post_recv_buffer() {
     recv_wr.sg_list = &recv_sge;
     recv_wr.num_sge = 1;
 
-    ret = ibv_post_recv(id->qp, &recv_wr, &bad_recv_wr);
-    if (ret) {
+    if (ibv_post_recv(id->qp, &recv_wr, &bad_recv_wr)) {
         perror("Failed to post receive work request");
         exit(EXIT_FAILURE);
     }
@@ -141,38 +144,34 @@ static void pre_post_recv_buffer() {
 }
 
 static void connect_server() {
-    int ret = -1;
     struct rdma_conn_param conn_param;
 
-    client_pdata.buf_va = (uintptr_t)recv_buffer;
-    client_pdata.buf_rkey = htonl(ctx.recv_mr->rkey);
+    rep_pdata.buf_va = (uintptr_t)recv_buffer;
+    rep_pdata.buf_rkey = htonl(ctx.recv_mr->rkey);
 
     memset(&conn_param, 0, sizeof(conn_param));
     conn_param.initiator_depth = 3;
     conn_param.responder_resources = 3;
     conn_param.retry_count = 3;
-    conn_param.private_data = &client_pdata; 
-    conn_param.private_data_len = sizeof(client_pdata);
+    conn_param.private_data = &rep_pdata; 
+    conn_param.private_data_len = sizeof(rep_pdata);
 
     printf("Connecting...\n");
-    ret = rdma_connect(id, &conn_param);
-    if (ret) {
+    if (rdma_connect(id, &conn_param)) {
         perror("Failed to connect to remote host");
         exit(EXIT_FAILURE);
     }
 
-    ret = rdma_get_cm_event(ec, &event);
-    if (ret) {
+    if (rdma_get_cm_event(ec, &event)) {
         perror("Failed to get cm event");
         exit(EXIT_FAILURE);
     }
     printf("Connection established.\n");
 
-    memcpy(&server_pdata,event->param.conn.private_data,sizeof(server_pdata));
-    printf("Received Server Memory at address %p with LKey %u\n\n",(void *)server_pdata.buf_va, ntohl(server_pdata.buf_rkey));
+    memcpy(&rep_pdata, event->param.conn.private_data, sizeof(rep_pdata));
+    printf("Received Server Memory at address %p with RKey %u\n\n",(void *)rep_pdata.buf_va, ntohl(rep_pdata.buf_rkey));
 
-    ret = rdma_ack_cm_event(event);
-    if (ret) {
+    if (rdma_ack_cm_event(event)) {
         perror("Failed to acknowledge cm event");
         exit(EXIT_FAILURE);
     }
@@ -183,20 +182,19 @@ int on_connect() {
     char command[256];
     struct message msg_send;
 
-    // 메모리 등록
     send_buffer = (char *)malloc(BUFFER_SIZE);
     if (!send_buffer) {
         perror("Failed to allocate memory for send buffer");
         exit(EXIT_FAILURE);
     }
 
-    ctx.send_mr = ibv_reg_mr(ctx.pd, send_buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE);
+    ctx.send_mr = ibv_reg_mr(ctx.pd, send_buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE 
+        | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE);
+    
     if (!ctx.send_mr) {
         fprintf(stderr, "Failed to register client metadata buffer.\n");
         exit(EXIT_FAILURE);
     }
-
-    //printf("Received Server Memory at address %p with LKey %u\n\n",(void *)server_pdata.buf_va, ntohl(server_pdata.buf_rkey));
 
     while (1) {
         printf("Enter command ( put k v / get k ): ");
@@ -204,7 +202,7 @@ int on_connect() {
             fprintf(stderr, "Error reading command\n");
             continue;
         }
-        command[strcspn(command, "\n")] = '\0'; // 개행 문자 제거
+        command[strcspn(command, "\n")] = '\0'; 
 
         char *cmd = strtok(command, " ");
 
@@ -236,128 +234,99 @@ int on_connect() {
             continue;
         }
 
+        msg_send.type = htonl(msg_send.type);
+    
         memcpy(send_buffer, &msg_send, sizeof(struct message));
-        struct message *msg_in_buffer = (struct message *)send_buffer;
-        printf("\nsend_buffer content:\n");
-        printf("Type: %d\n", msg_in_buffer->type);
-        printf("Key: %s\n", msg_in_buffer->kv.key);
-        printf("Value: %s\n\n", msg_in_buffer->kv.value);
-
-
-        // 메시지 전송
+    
         post_send_message();
 
-        // 서버로부터 응답 수신
-        receive_response();
+        if (receive_response() != 0) {
+            fprintf(stderr, "Failed to receive response\n");
+            continue;
+        }
     }
 
     cleanup(id);
     return 0;
 }
+
 void post_send_message() {
-    printf("post_send_message\n");
+    struct message *msg_send = (struct message *)send_buffer;
+
     send_sge.addr = (uintptr_t)send_buffer;
     send_sge.length = sizeof(send_buffer);
     send_sge.lkey = ctx.send_mr->lkey;
 
-    send_wr.opcode = IBV_WR_RDMA_WRITE;
-    send_wr.wr_id = 1;
+    send_wr.wr_id = 2;
     send_wr.sg_list = &send_sge;
     send_wr.num_sge = 1;
     send_wr.send_flags = IBV_SEND_SIGNALED;
-    send_wr.wr.rdma.rkey = ntohl(server_pdata.buf_rkey); // 서버 권한 키
-	send_wr.wr.rdma.remote_addr = bswap_64(server_pdata.buf_va); // 서버 메모리 주소
 
-    //printf("Received Server Memory at address %p with LKey %u\n\n", send_wr.wr.rdma.rkey, send_wr.wr.rdma.remote_addr);
-    //printf("Received Server Memory at address %p with LKey %u\n\n",(void *)server_pdata.buf_va, server_pdata.buf_rkey);
-   
-    if (ibv_post_send(id->qp, &send_wr, &bad_send_wr)) {
-        fprintf(stderr, "Failed to post send work request: %s\n", strerror(errno));
+    send_wr.opcode = IBV_WR_RDMA_WRITE;
+    send_wr.wr.rdma.rkey = ntohl(rep_pdata.buf_rkey); 
+	send_wr.wr.rdma.remote_addr = ntohll(rep_pdata.buf_va); 
+
+    struct message *msg_in_buffer = (struct message *)send_buffer;
+    printf("\nsend_buffer content:\n");
+    printf("Type: %d\n", msg_in_buffer->type);
+    printf("Key: %s\n", msg_in_buffer->kv.key);
+    printf("Value: %s\n\n", msg_in_buffer->kv.value);
+
+
+    if (post_and_wait(&send_wr, "RDMA Write") != 0) {
         exit(EXIT_FAILURE);
     }
 
-    struct ibv_wc wc;
-    int num_wc = ibv_poll_cq(ctx.qp->send_cq, 1, &wc);
-    if (num_wc < 0) {
-        fprintf(stderr, "Error polling CQ: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    } else if (num_wc == 0) {
-        fprintf(stderr, "No completion found in CQ\n");
+    send_wr.opcode = IBV_WR_SEND;
+    send_sge.length = sizeof(uint32_t);
+
+    if (post_and_wait(&send_wr, "Send") != 0) {
         exit(EXIT_FAILURE);
     }
-
-    if (wc.status != IBV_WC_SUCCESS) {
-        fprintf(stderr, "Send failed with status %s\n", ibv_wc_status_str(wc.status));
+    if (receive_response() != 0) {
+        fprintf(stderr, "Failed to receive response\n");
         exit(EXIT_FAILURE);
     }
-
-    // if (ibv_poll_cq(ctx.qp->send_cq, 1, &wc) > 0) {
-    //     if (wc.status != IBV_WC_SUCCESS) {
-    //         fprintf(stderr, "Send failed with status %s\n", ibv_wc_status_str(wc.status));
-    //         exit(EXIT_FAILURE);
-    //     }
-    // }
 }
 
-void receive_response() {
-    printf("receive_response\n");
-    // void *cq_context;
-    // int end_loop = 0;
-    // while(!end_loop) {
-    //     if (ibv_get_cq_event(ctx.comp_channel,&ctx.evt_cq,&cq_context))
-	// 		return 1;
-    //     printf("ibv_get_cq_event\n");
-
-	// 	if (ibv_req_notify_cq(ctx.cq,0))
-	// 		return 1;
-	// 	printf("ibv_req_notify_cq\n");
-        
-    //     if (ibv_poll_cq(ctx.qp, 1, &wc) != 1)
-	// 		return 1;
-	// 	printf("ibv_poll_cq\n");
-        
-    //     if (wc.status != IBV_WC_SUCCESS)
-	// 		return 1;
-
-    //     printf("switch~~~\n");    
-	// 	switch (wc.wr_id) {
-	// 	    case 0:
-	// 		    printf("server ans : %d\n", ntohl(recv_buffer[0]));
-	// 		    end_loop = 1;
-	// 		    break;
-	// 	    case 1:
-	// 		    if (prepare_send_notify_after_rdma_write())
-	// 			    return 1;
-	// 		    break;
-	// 	    default:
-	// 		    end_loop = 1;
-	// 		    break;
-	// 	}
-    // }
-    // ibv_ack_cq_events(ctx.cq,2);
-    int ret;
-
-    recv_wr.sg_list = &recv_sge;
-    recv_wr.num_sge = 1;
-
-    ret = ibv_post_recv(ctx.qp, &recv_wr, &bad_recv_wr);
-    if (ret) {
-        fprintf(stderr, "Failed to post receive work request: %s\n", strerror(errno));
+int post_and_wait(struct ibv_send_wr *wr, const char *operation_name) {
+    if (ibv_post_send(id->qp, wr, &bad_send_wr)) {
+        fprintf(stderr, "Failed to post %s work request: %s\n", operation_name, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    struct ibv_wc wc;
-    int num_wc = ibv_poll_cq(ctx.qp->recv_cq, 1, &wc);
-    if (num_wc < 0) {
-        fprintf(stderr, "Error polling CQ: %s\n", strerror(errno));
+    if (wait_for_completion() != 0) {
+        fprintf(stderr, "%s operation failed\n", operation_name);
         exit(EXIT_FAILURE);
-    } else if (num_wc == 0) {
-        fprintf(stderr, "No completion found in CQ\n");
+    }
+
+    printf("%s completed successfully\n\n", operation_name);
+    return 0;
+}
+
+
+int wait_for_completion() {
+    struct ibv_wc wc;
+    int ret;
+
+    while ((ret = ibv_poll_cq(ctx.cq, 1, &wc)) == 0);
+
+    if (ret < 0) {
+        fprintf(stderr, "Failed to poll CQ: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     if (wc.status != IBV_WC_SUCCESS) {
-        fprintf(stderr, "Receive failed with status %s\n", ibv_wc_status_str(wc.status));
+        fprintf(stderr, "WR failed with status %s\n", ibv_wc_status_str(wc.status));
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+int receive_response() {
+    if (wait_for_completion() != 0) {
+        fprintf(stderr, "Failed to receive response\n");
         exit(EXIT_FAILURE);
     }
 
@@ -367,34 +336,36 @@ void receive_response() {
     } else {
         printf("Response: %s\n", response->kv.value);
     }
+
+    return 0;
 }
 
-int prepare_send_notify_after_rdma_write()
-{
-	struct ibv_sge					sge; 
-   	struct ibv_send_wr				send_wr = { }; 
-   	struct ibv_send_wr 				*bad_send_wr; 
+// int send_notification_to_server() {
+//     struct ibv_sge sge;
+//     struct ibv_send_wr send_wr = {}, *bad_send_wr = NULL;
+//     char notification = 1;
 
-	uint8_t *buf = calloc(1, sizeof(uint8_t));
-	struct ibv_mr *mr = ibv_reg_mr(ctx.pd, buf, sizeof(uint8_t), IBV_ACCESS_LOCAL_WRITE); 
-	if (!mr) 
-		return 1;
-	
-	sge.addr = (uintptr_t)buf; 
-    sge.length = sizeof(uint8_t); 
-    sge.lkey = mr->lkey;
-    
-	memset(&send_wr, 0, sizeof(send_wr));
-	send_wr.wr_id = 2;
-    send_wr.opcode = IBV_WR_SEND;
-    send_wr.sg_list = &sge;
-    send_wr.num_sge = 1;
+//     struct ibv_mr *notify_mr = ibv_reg_mr(ctx.pd, &notification, sizeof(char), IBV_ACCESS_LOCAL_WRITE);
+//     if (!notify_mr) {
+//         fprintf(stderr, "Failed to register MR for notification: %s\n", strerror(errno));
+//         return -1;
+//     }
 
-    if (ibv_post_send(id->qp,&send_wr,&bad_send_wr)) 
-        return 1;
+//     sge.addr = (uintptr_t)&notification;
+//     sge.length = sizeof(char);
+//     sge.lkey = notify_mr->lkey;
 
-	return 0;
-}
+//     send_wr.wr_id = 2;
+//     send_wr.opcode = IBV_WR_SEND;
+//     send_wr.sg_list = &sge;
+//     send_wr.num_sge = 1;
+//     send_wr.send_flags = IBV_SEND_SIGNALED;
+
+//     int result = post_and_wait(&send_wr, "Notification");
+
+//     ibv_dereg_mr(notify_mr);
+//     return result;
+// }
 
 void cleanup(struct rdma_cm_id *id) {
 
@@ -449,15 +420,17 @@ void cleanup(struct rdma_cm_id *id) {
     }
 }
 
-/*
-./client 10.10.1.2
-Creating QP...
-Queue Pair created: 0x55f6de161018
 
-Memory registered at address 0x55f6de1618c0 with LKey 143633
+
+/*
+./client 10.10.1.1
+Creating QP...
+Queue Pair created: 0x55bcadf7a018
+
+Memory registered at address 0x55bcadf7a8c0 with LKey 103539
 Connecting...
 Connection established.
-Received Server Memory at address 0x5647122269e0 with LKey 127143
+Received Server Memory at address 0xe089c890ae550000 with RKey 71933
 
 The client is connected successfully. 
 
@@ -469,7 +442,9 @@ Type: 0
 Key: a
 Value: b
 
-post_send_message
-No completion found in CQ
+RDMA Write completed successfully
 
+Send completed successfully
+
+WR failed with status local length error
 */
