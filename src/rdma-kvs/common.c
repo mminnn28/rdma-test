dc71ffbcab8b2c2335eb0e6861615761fcb66d5a
jeongeun Kim <rlawjd10eun@gmail.com>
2024-09-02 22:41:17 +0900

    Update common.c

--- src/rdma-kvs/common.c
+++ src/rdma-kvs/common.c
@@ -1,46 +1,57 @@
 #include "common.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <rdma/rdma_cma.h>
 
-void build_context(struct ibv_context *verbs, struct rdma_context *ctx) {
-    ctx->ctx = verbs;
-    ctx->pd = ibv_alloc_pd(ctx->ctx);
-    ctx->comp_channel = ibv_create_comp_channel(ctx->ctx);
-    ctx->cq = ibv_create_cq(ctx->ctx, 10, NULL, ctx->comp_channel, 0);
-    ibv_req_notify_cq(ctx->cq, 0);
+void build_context(struct rdma_context *ctx, struct rdma_cm_id *id) {
+    // device
+    struct ibv_device **device_list = ibv_get_device_list(NULL);
+    ctx->device = device_list[0]; 
+    ctx->verbs = ibv_open_device(ctx->device); 
+    ibv_free_device_list(device_list); 
+
+    // resource
+    ctx->pd = ibv_alloc_pd(id->verbs);
+    if (!ctx->pd) {
+        perror("ibv_alloc_pd");
+        exit(EXIT_FAILURE);
+	}
+    //id->context = (void *)malloc(sizeof(struct rdma_context));
+
+    ctx->comp_channel = ibv_create_comp_channel(id->verbs); 
+    if (!ctx->comp_channel) {
+        perror("ibv_create_comp_channel");
+        exit(EXIT_FAILURE);
+	}
+
+    ctx->cq = ibv_create_cq(id->verbs, CQ_CAPACITY, NULL, ctx->comp_channel, 0);
+    if (!ctx->cq) {
+        perror("ibv_create_cq");
+        exit(EXIT_FAILURE);
+	}
+
+    if (ibv_req_notify_cq(ctx->cq, 0)) {
+        perror("ibv_req_notify_cq");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void build_qp_attr(struct ibv_qp_init_attr *qp_attr, struct rdma_context *ctx) {
-    memset(qp_attr, 0, sizeof(*qp_attr));
-    qp_attr->send_cq = ctx->cq;
-    qp_attr->recv_cq = ctx->cq;
-    qp_attr->qp_type = IBV_QPT_RC;
-    qp_attr->cap.max_send_wr = 10;
-    qp_attr->cap.max_recv_wr = 10;
-    qp_attr->cap.max_send_sge = 1;
-    qp_attr->cap.max_recv_sge = 1;
+void build_qp_attr(struct ibv_qp_init_attr *attr, struct rdma_context *ctx) {
+
+    memset(attr, 0, sizeof(*attr));
+    attr->qp_type = IBV_QPT_RC;
+
+    attr->cap.max_send_wr = MAX_WR;
+    attr->cap.max_recv_wr = MAX_WR;
+    attr->cap.max_send_sge = MAX_SGE;
+    attr->cap.max_recv_sge = MAX_SGE;
+
+    if (!ctx->cq) {
+        fprintf(stderr, "Completion Queue (CQ) is not initialized.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    attr->send_cq = ctx->cq;
+    attr->recv_cq = ctx->cq;
+
+    attr->srq = NULL;
+    attr->sq_sig_all = 0;
 }
 
-void register_memory(struct rdma_context *ctx) {
-    ctx->buffer = malloc(BUFFER_SIZE);
-    ctx->mr = ibv_reg_mr(ctx->pd, ctx->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ);
-}
-
-void post_receives(struct rdma_context *ctx) {
-    struct ibv_sge sge;
-    struct ibv_recv_wr wr, *bad_wr = NULL;
-
-    sge.addr = (uintptr_t)ctx->buffer;
-    sge.length = BUFFER_SIZE;
-    sge.lkey = ctx->mr->lkey;
-
-    memset(&wr, 0, sizeof(wr));
-    wr.wr_id = (uintptr_t)ctx;
-    wr.sg_list = &sge;
-    wr.num_sge = 1;
-
-    ibv_post_recv(ctx->qp, &wr, &bad_wr);
-}
